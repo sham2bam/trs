98cf4349158e68944921c2665610faf58e7b9df6
Shammer Diaz <shammer.diaz@gmail.com>
2015-09-04 06:45:56 -0400

    git pull

--- front-page.php
+++ front-page.php
@@ -32,17 +32,35 @@
   </div>
 
   <div class="row">
-    <div class="container about-us ">
-      <div class="col-md-fw-4 col-md-offset-4 content-img vertc-parent">
+    <div class="container services">
+      <div class="col-md-fw-4 content-img vertc-parent">
         <img class="vertc-child" src="http://asoexpebla.com/wp-content/uploads/2015/08/west-elm-living-room-popular-with-photo-of-west-elm-set-new-at-gallery.jpg">
       </div>
-      <div class="col-md-8 col-md-offset-8 u-border-top vertc-parent">
+      <div class="col-md-8 pull-content u-border-top vertc-parent">
         <div class="vertc-child">
-          <h3>About Us</h3>
-          <p>
-            We provide living, organizing, designing and staging services for clients at all price points, ranging from turnkey solutions (home improvements, deep cleaning, organizing, staging, design or redesign) to individual solutions (home office design, individual areas or specific services). We maintain relationships with local contractors, service providers, artists and interior designers that can bring in specialty services and trades to meet our client’s needs.
-          </p>
-          <a href="/" class="button btn btn-primary">Read More</a>
+          <h3>Our Services</h3>
+          <div class="col-md-8">
+            <p>
+              Many people invest in staging and organizing just in time to sell and leave the home. Why wait until you will sell your house in order for the next owner enjoy all those improvements? Why wait until you leave to improve your home, when you can enjoy being in an organized, clean, personally designed home throughout the time you live there?
+            </p>
+            <a href="/" class="button btn btn-primary">Read More</a>
+          </div>
+          <div class="col-md-4">
+            <ul>
+              <li>
+                staging
+              </li>
+              <li>
+                organization
+              </li>
+              <li>
+                cleaning
+              </li>
+              <li>
+                interior design
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- inc/version.php
+++ inc/version.php
@@ -1,4 +1,4 @@
 <?php
   global $package_version;
-  $package_version = '2015-09-03T11:47:13.009Z';
+  $package_version = '2015-09-04T03:27:55.486Z';
 ?>
